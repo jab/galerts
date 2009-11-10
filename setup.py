0b8a8050a86f959d1573166d211a39bcb128c9a6
Joshua Bronson <jabronson@gmail.com>
2009-11-10 16:40:07 -0500

    rm ez_setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from ez_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup, find_packages
+from setuptools import setup, find_packages
 
 version = '0.1.3'
 try:
