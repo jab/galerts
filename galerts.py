64618a4f1e7a57a13e568fe31e468da99546367e
Joshua Bronson <jabronson@gmail.com>
2011-01-05 17:10:28 -0500

    update copyright to 2011

--- galerts.py
+++ galerts.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2010 Josh Bronson and contributors
+# Copyright (c) 2011 Josh Bronson
 # 
 # Permission is hereby granted, free of charge, to any person
 # obtaining a copy of this software and associated documentation
